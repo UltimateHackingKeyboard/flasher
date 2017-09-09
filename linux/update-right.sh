f2de3fea4cf240b6291981f197af993c34ea2f08
László Monda <laci@monda.hu>
2017-09-10 01:01:45 +0200

    Use the newly created architecture-independent blhost script.

--- linux/update-right.sh
+++ linux/update-right.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e # fail the script if a command fails
 
-blhost --usb 0x1d50,0x6120 flash-security-disable 0403020108070605
-blhost --usb 0x1d50,0x6120 flash-erase-region 0xc000 475136
-blhost --usb 0x1d50,0x6120 flash-image ../shared/uhk-right.srec
-blhost --usb 0x1d50,0x6120 reset
+bin/blhost --usb 0x1d50,0x6120 flash-security-disable 0403020108070605
+bin/blhost --usb 0x1d50,0x6120 flash-erase-region 0xc000 475136
+bin/blhost --usb 0x1d50,0x6120 flash-image ../shared/uhk-right.srec
+bin/blhost --usb 0x1d50,0x6120 reset
