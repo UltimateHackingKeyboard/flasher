5696cc7eeb2cfb9d35dbfd8e55346a1d9b217fe7
Laszlo Monda <laci@monda.hu>
2017-11-18 22:25:01 +0000

    Fix the path of firmware.hex

--- linux/update-right.sh
+++ linux/update-right.sh
@@ -5,5 +5,5 @@ current_dir=`dirname $0`
 blhost=$current_dir/bin/blhost
 $blhost --usb 0x1d50,0x6120 flash-security-disable 0403020108070605
 $blhost --usb 0x1d50,0x6120 flash-erase-region 0xc000 475136
-$blhost --usb 0x1d50,0x6120 flash-image $current_dir/../shared/firmware/devices/uhk60-right/firmware.hex
+$blhost --usb 0x1d50,0x6120 flash-image $current_dir/../firmware/devices/uhk60-right/firmware.hex
 $blhost --usb 0x1d50,0x6120 reset
