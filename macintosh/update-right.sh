c979c60a9daa8f15d81f1450da9daf732e7010e7
Róbert Kiss <ert78gb@gmail.com>
2017-11-22 20:33:05 +0100

    feat(mac): Make the mac scripts use the firmware directory

--- macintosh/update-right.sh
+++ macintosh/update-right.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e # fail the script if a command fails
 
-current_dir=`dirname $0`
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+current_dir=`realpath $(dirname $0)`
 blhost=$current_dir/bin/blhost
 $blhost --usb 0x1d50,0x6120 flash-security-disable 0403020108070605
 $blhost --usb 0x1d50,0x6120 flash-erase-region 0xc000 475136
-$blhost --usb 0x1d50,0x6120 flash-image $current_dir/../shared/uhk-right.hex
+$blhost --usb 0x1d50,0x6120 flash-image $current_dir/../firmware/devices/uhk60-right/firmware.hex
 $blhost --usb 0x1d50,0x6120 reset
